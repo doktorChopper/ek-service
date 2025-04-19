da6632986f43c3e833e9dedebc9c27153f81acc7
Chopper <doc.chopperx@gmail.com>
2025-04-20 01:31:49 +0300

    added AuthMiddleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,61 +1,32 @@
 package middleware
 
 import (
+	"log"
 	"net/http"
-	"time"
+	// "time"
 
-	"github.com/doktorChopper/ek-service/internal/models"
+	// "github.com/doktorChopper/ek-service/internal/models"
 	"github.com/doktorChopper/ek-service/internal/store"
 )
 
 
 func AuthMiddleware(s *store.SessionStore, next http.HandlerFunc) http.HandlerFunc {
     return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {
-        password := r.FormValue("password")
-        email := r.FormValue("login")
 
-        if password == "" || email == "" {
-            http.Redirect(w, r, "/login", http.StatusSeeOther)
-        }
-
-        var checkPassword string
-        var id int
-
-        // stmt := `SELECT id, hashed_password FROM users WHERE email = $1` 
-
-        // row := s.QueryRow(stmt, email)
-        // row.Scan(&id, &checkPassword)
-
-        flag := true
-
-        if len(checkPassword) != len(password) {
-            flag = false
-        }
-
-        for i := range checkPassword {
-            if checkPassword[i] != password[i] {
-               flag = false
-            } 
-        }
-
-        if !flag {
+        cookie, err := r.Cookie("session_token")
+        if err != nil {
+            // http.Error(w, "No cookie", http.StatusInternalServerError)
+            log.Println(err.Error())
             http.Redirect(w, r, "/login", http.StatusSeeOther)
             return
-        } else {
-            session := models.NewSession(id, 120 * time.Second)
+        }
 
-            err := s.Create(session)
-            if err != nil {
-                return
-            }
-
-            cookie := http.Cookie {
-                Name:       "session_token",
-                Value:      session.ID,
-                Expires:    session.ExpiresAt,
-            }
-
-            http.SetCookie(w, &cookie)
+        _, err = s.Get(cookie.Value)
+        if err != nil {
+            // http.Error(w, "No session", http.StatusInternalServerError)
+            log.Println(err.Error())
+            http.Redirect(w, r, "/login", http.StatusSeeOther)
+            return
         }
 
         next.ServeHTTP(w, r)
